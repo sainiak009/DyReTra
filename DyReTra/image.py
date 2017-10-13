10f843585c66f766b17415b70666c8f2a5c6751d
punit1108 <punitkpal@gmail.com>
2017-10-13 16:29:02 +0530

    final push for presentation

--- DyReTra/image.py
+++ DyReTra/image.py
@@ -12,16 +12,24 @@ colors = [
     [0, 255, 160, 0, 255, 165, 10]
 ]
 
+# Recieves the snapshot to process it and extracts meaningful data out of the image
+# @param {Dictionary} Configuration data of working cluster
+# @param {String} Image name
+# @returns {List} List of Integers signifying Traffic Density
+
 def getTrafficData(json_data, image = "default.png"):
     roads = parseClusterData(json_data)
 
+    # Reads the Image and converts it to matrix
     snapshot = cv2.imread("trafficSnaps/" + image , -1)
 
+    # Tries to convert the imaged to HSV from BGR
     try:
         snapshot_hsv = cv2.cvtColor(snapshot, cv2.COLOR_BGR2HSV)
     except Exception as e:
         print("CV2 Failure")
         exit()
+
     snapshot_res = []
     road_data = []
 
@@ -37,11 +45,16 @@ def getTrafficData(json_data, image = "default.png"):
         gray = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
         minLineLength = 50
         maxLineGap = 50
+
+        # Uses Hough Lines Analysis to Detect Lines
+
         lines = cv2.HoughLinesP(gray, 5, np.pi / 180, 1000, minLineLength=minLineLength, maxLineGap=maxLineGap)
         snapshot_res.append(lines)
 
     # print (snapshot_res)
 
+    # Iteration to extract data
+    
     for road in roads:
         print(road)
         if road[0] - road[2] == 0:
@@ -66,6 +79,8 @@ def getTrafficData(json_data, image = "default.png"):
                         cv_road_m = ((cv_road[1] - cv_road[3]) / (cv_road[0] - cv_road[2]))
                         cv_road_th = math.degrees(math.atan(cv_road_m))
                         cv_road_d = (cv_road_m * cv_road[0] - cv_road[1]) / math.sqrt(cv_road_m ** 2 + 1)
+
+                    # Tests the proximity of Lines
 
                     if math.fabs(cv_road_th - road_th) < 5 and math.fabs(cv_road_d - road_d) == 0:
                         road_sig = color_i
